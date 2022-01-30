19304476f84e749c6738a0b7c41cc967afd16348
JonathanCU <jonathan.cu95@gmail.com>
2022-01-29 18:34:44 -0600

    Add classes config-cloud-local

---
+++ config-cloud-local/src/main/java/com/solutions/factory/configserver/ConfigCloudLocalApplication.java
@@ -0,0 +1,15 @@
+package com.solutions.factory.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigCloudLocalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigCloudLocalApplication.class, args);
+	}
+
+}
