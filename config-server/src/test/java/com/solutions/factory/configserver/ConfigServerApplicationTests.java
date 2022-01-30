ca654e5974a905bf16cc7a9b57fcb3607b4dc88f
Jonathan Cisneros <jonathan.cu95@gmail.com>
2022-01-29 21:12:29 -0600

    Add config-server git project

---
+++ config-server/src/test/java/com/solutions/factory/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solutions.factory.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
